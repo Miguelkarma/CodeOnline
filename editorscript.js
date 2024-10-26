b18372b9bfb7fd67f755199acff74eb45e952d42
Miguelkarma <santos.p.miguel46@gmail.com>
2024-10-26 20:29:32 +0800

    third commit

--- editorscript.js
+++ editorscript.js
@@ -1,11 +1,11 @@
-function run(){
+function compile(){
 
     let htmlCode = document.getElementById("html").value;
     let cssCode = document.getElementById("css").value;
     let jsCode = document.getElementById("js").value;
+
     let outPut = document.getElementById("output");
 
     outPut.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
     outPut.contentWindow.eval(jsCode);
-
 }
